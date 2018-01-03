58ca8e03234094c56e3bdfd42f6fdfbfab2828d9
pinglinh <lnguyenmy1@gmail.com>
2018-01-03 14:46:04 +0000

    Finished tutorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import { map } from 'lodash';
 
-console.log(map([1,2,3,4,5,6], n => n*n));
+let numbers = map([1,2,3,4,5,6], n => n*n);
+
+setTimeout(() => {
+  require(['./numberlist.hbs'], template => {
+    document.getElementById("app-container").innerHTML = template({numbers});
+  })
+}, 2000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
 const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
-  entry: [
-    'babel-polyfill',
-    './src/main.js'
-  ],
+  entry: {
+    vendor: ['babel-polyfill', 'lodash'],
+    main: './src/main.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-
+          plugins: ['transform-runtime'],
           presets: ['es2015']
-        }
-      }
-    ]
-  }
+        },
+        test: /\.hbs$/,
+        loader: 'handlebars-loader'
+      }]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Intro to webpack',
+      template: 'src/index.html'
+    }),
+    new UglifyJsPlugin({
+      beautify: false,
+      mangle: { screw_ie8 : true },
+      compress: { screw_ie8: true, warnings: false },
+      comments: false
+    }),
+    new CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js'
+    })
+  ]
 }
